5931d582117d9e187408ebdad519216b1ac88cb6
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-07-27 11:27:50 +0300

    nextjs links

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nexjs-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something Else</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
